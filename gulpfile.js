9620ec7d895243fad0876d80a5c4b1c78f0c98be
Alex <barbeitoalex@gmail.com>
2019-01-21 15:11:19 +0100

    gulp fix + implement stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require("gulp");
 var nodemon = require("gulp-nodemon");
 var ts = require("gulp-typescript");
+var stylus = require("gulp-stylus");
 var sourcemaps = require('gulp-sourcemaps');
 
 // Prepare Typescript configuration
 var tsProject = ts.createProject('tsconfig.json');
 
 // Compile Typescript
-gulp.task('build', function () {
+gulp.task('tsc', function () {
     return tsProject.src()
         .pipe(sourcemaps.init({
             loadMaps: true
@@ -17,21 +18,35 @@ gulp.task('build', function () {
         .pipe(gulp.dest("dist"));
 });
 
+// Compile Stylus
+gulp.task('stylus', function () {
+    return gulp.src('style/main.styl')
+        .pipe(stylus({
+            compress: true
+        }))
+        .pipe(gulp.dest('public/resource/css'));
+});
+
+// Build
+gulp.task('build', gulp.parallel('tsc', 'stylus'));
+
+// Watch changes for Typescript and Stylus files and compile
+gulp.task('watch', gulp.series('build', function () {
+    gulp.watch('src/**/*.ts')
+        .on('change', gulp.series('tsc'))
+        .on('unlink', gulp.series('tsc'));
+    gulp.watch('style/main.styl')
+        .on('change', gulp.series('stylus'));
+}));
+
 // Build task and Start Nodemon
 gulp.task('start', gulp.series('build', function () {
     return nodemon({
         script: 'dist/index.js',
-        watch: 'dist'
+        watch: ['src/*.*', 'style/main.styl'],
+        tasks: 'build'
     });
 }));
 
-// Start task and Watch changes for Typescript files and compile
-gulp.task('watch', gulp.series('start', function () {
-    gulp.watch('src/*.ts')
-    .on('change', gulp.series('build'))
-    .on('unlink', gulp.series('build'));
-}));
-
 // Default
-gulp.task('default', gulp.series('watch'));
-
+gulp.task('default', gulp.series('start'));
