bdaf58d2a2d4128332a483a80bb1826550ad9851
Alex <barbeitoalex@gmail.com>
2019-01-21 12:21:42 +0100

    implement gulp

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require("gulp");
+var nodemon = require("gulp-nodemon");
+var ts = require("gulp-typescript");
+var sourcemaps = require('gulp-sourcemaps');
+
+// Prepare Typescript configuration
+var tsProject = ts.createProject('tsconfig.json');
+
+// Compile Typescript
+gulp.task('build', function () {
+    return tsProject.src()
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe(tsProject()).js
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest("dist"));
+});
+
+// Build task and Start Nodemon
+gulp.task('start', gulp.series('build', function () {
+    return nodemon({
+        script: 'dist/index.js',
+        watch: 'dist'
+    });
+}));
+
+// Start task and Watch changes for Typescript files and compile
+gulp.task('watch', gulp.series('start', function () {
+    gulp.watch('src/*.ts')
+    .on('change', gulp.series('build'))
+    .on('unlink', gulp.series('build'))
+}))
+
+// Default
+gulp.task('default', gulp.series('watch'));
+
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import Server from './server';
-import setSockets from './socket/index';
 
 const PORT: number = 3000;
 const express = new Server(PORT);
